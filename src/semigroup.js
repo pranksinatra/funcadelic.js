30cf0e2969fa16ccfb4f7485b0b512d1c99c2487
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2019-03-26 22:58:02 -0700

    Add explicit class name for Semigroup
    Hoping to fix "invalid typeclass name" error after uglification

--- src/semigroup.js
+++ src/semigroup.js
@@ -1,6 +1,7 @@
 import { type } from './typeclasses';
 
 export const Semigroup = type(class Semigroup {
+  static name = 'Semigroup';
   append(left, right) {
     let { append } = this(left);
     return append(left, right);
